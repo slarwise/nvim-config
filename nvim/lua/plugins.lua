a76fea4091be484555a008f8492983326a2f0f5f
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2021-12-25 16:28:45 +0100

    Add lua to markdown_fenced_languages

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,7 +3,7 @@ vim.g.tex_no_error = 1
 vim.g.tex_flavor = "latex"
 vim.g.tex_comment_nospell = 1
 vim.g.vimsyn_embed = "l" -- Syntax highlighting for lua in vim files
-vim.g.markdown_fenced_languages = { "python", "vim" }
+vim.g.markdown_fenced_languages = { "python", "vim", "lua", }
 
 vim.cmd([[
   augroup packer_user_config
