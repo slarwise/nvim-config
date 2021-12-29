57bd478445b44026d6a148e8872875e8ac440d54
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2021-12-29 20:08:45 +0100

    Remove unused field in lua lsp setup

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -60,8 +60,6 @@ nvim_lsp.sumneko_lua.setup({
             runtime = {
                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = "LuaJIT",
-                -- Setup your lua path
-                path = runtime_path,
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
