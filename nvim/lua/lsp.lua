63243cf764417fcad8717ab3bc3e20a0990db592
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2021-12-29 20:03:45 +0100

    Format lsp.lua using stylua

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,42 +1,43 @@
-local nvim_lsp = require('lspconfig')
-local null_ls = require"null-ls"
+local nvim_lsp = require("lspconfig")
+local null_ls = require("null-ls")
 local helpers = require("null-ls.helpers")
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+    local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+    local function buf_set_option(...)
+        vim.api.nvim_buf_set_option(bufnr, ...)
+    end
 
     --Enable completion triggered by <c-x><c-o>
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
     -- Mappings.
-    local opts = { noremap=true, silent=true }
+    local opts = { noremap = true, silent = true }
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'Xn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', 'Xa', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', 'Qr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', 'Qd', '<cmd>lua vim.lsp.diagnostic.set_qflist()<CR>', opts)
+    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    buf_set_keymap("n", "Xn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    buf_set_keymap("n", "Xa", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+    buf_set_keymap("n", "Qr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    buf_set_keymap("n", "Qd", "<cmd>lua vim.lsp.diagnostic.set_qflist()<CR>", opts)
     buf_set_keymap("n", "Xf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
     local do_format_on_write = not vim.tbl_contains({ "erlangls" }, client.name)
-    if (do_format_on_write and client.resolved_capabilities.document_formatting) then
+    if do_format_on_write and client.resolved_capabilities.document_formatting then
         vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
     end
-
 end
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-vim.lsp.handlers.hover, {
-    border = "rounded"
-}
-)
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = "rounded",
+})
 
-vim.diagnostic.config({signs = false})
+vim.diagnostic.config({ signs = false })
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
@@ -49,22 +50,22 @@ vim.diagnostic.config({signs = false})
 --         },
 --     }
 -- end
-nvim_lsp.erlangls.setup {
+nvim_lsp.erlangls.setup({
     on_attach = on_attach,
     flags = { debounce_text_changes = 150 },
-    }
-nvim_lsp.sumneko_lua.setup {
+})
+nvim_lsp.sumneko_lua.setup({
     settings = {
         Lua = {
             runtime = {
                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
+                version = "LuaJIT",
                 -- Setup your lua path
                 path = runtime_path,
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
-                globals = {'vim'},
+                globals = { "vim" },
             },
             workspace = {
                 -- Make the server aware of Neovim runtime files
@@ -76,7 +77,7 @@ nvim_lsp.sumneko_lua.setup {
             },
         },
     },
-}
+})
 
 local plantuml = {
     name = "plantuml",
@@ -95,9 +96,9 @@ local plantuml = {
             local success = code <= 1
 
             if not success then
-              -- can be noisy for things that run often (e.g. diagnostics), but can
-              -- be useful for things that run on demand (e.g. formatting)
-              print(stderr)
+                -- can be noisy for things that run often (e.g. diagnostics), but can
+                -- be useful for things that run on demand (e.g. formatting)
+                print(stderr)
             end
 
             return success
@@ -115,7 +116,9 @@ local plantuml = {
 }
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.prettier.with{ args = { "--stdin-filepath", "--prose-wrap=always", "$FILENAME" } },
+        null_ls.builtins.formatting.prettier.with({
+            args = { "--stdin-filepath", "--prose-wrap=always", "$FILENAME" },
+        }),
         null_ls.builtins.diagnostics.markdownlint,
         plantuml,
         null_ls.builtins.diagnostics.vale,
