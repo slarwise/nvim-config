70cbb5635add10b9e7905aacb210fa0376dbd8bc
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2022-01-20 21:47:23 +0100

    Add simpler "icons" for notify

--- lua/my/plugins/notify.lua
+++ lua/my/plugins/notify.lua
@@ -1,6 +1,15 @@
 require("packer").use {
     "rcarriga/nvim-notify",
     config = function()
+        require("notify").setup {
+            icons = {
+                ERROR = "ERROR",
+                WARN = "WARNING",
+                INFO = "INFO",
+                DEBUG = "DEBUG",
+                TRACE = "TRACE",
+            },
+        }
         vim.notify = require "notify"
     end,
 }
