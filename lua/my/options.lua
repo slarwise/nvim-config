371accd40cc8c4b5c077fac95281d755d27d0862
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2022-01-20 21:54:23 +0100

    WIP: heirline statusline

--- lua/my/options.lua
+++ lua/my/options.lua
@@ -18,38 +18,6 @@ vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.list = true
 vim.cmd [[set wildcharm=<Tab>]]
-vim.cmd [[
-" Use a statusline with some fancy symbols if the terminal can handle it.
-" Also need to have a font which can handle the symbols.
-if exists("$COLORTERM")
-    function s:UpdateUserHLGroups() abort
-        if synIDattr(synIDtrans(hlID('StatusLine')), 'reverse')
-            let status_line_fg = 'fg'
-        else
-            let status_line_fg = 'bg'
-        end
-        if synIDattr(synIDtrans(hlID('StatusLineNC')), 'reverse')
-            let status_line_fg_not_current = 'fg'
-        else
-            let status_line_fg_not_current = 'bg'
-        end
-        let statusline_bg_current = synIDattr(synIDtrans(hlID('StatusLine')), status_line_fg)
-        let statusline_bg_not_current = synIDattr(synIDtrans(hlID('StatusLineNC')), status_line_fg_not_current)
-        let normal_bg = synIDattr(synIDtrans(hlID('Normal')), 'bg')
-        execute 'highlight User1 guifg=' .. statusline_bg_current .. ' guibg=' .. normal_bg
-        execute 'highlight User2 guifg=' .. statusline_bg_not_current .. ' guibg=' .. normal_bg
-    endfunction
-    " Update the highlight groups of the corners to get a rounded statusline
-    augroup statusline
-        autocmd!
-        autocmd ColorScheme * call s:UpdateUserHLGroups()
-        autocmd WinEnter,BufEnter * setlocal statusline=%1*%*\ %{&modified?'●':'\ '}\ \ %f%=%l/%L,%c\ %1*%*
-        autocmd WinLeave,BufLeave * setlocal statusline=%2*%*\ %{&modified?'●':'\ '}\ \ %f%=%l/%L,%c\ %2*%*
-    augroup END
-else
-    set statusline=\ %m\ \ %f%=%l/%L,%c\ 
-endif
-]]
 
 if vim.env.COLORTERM then
     vim.opt.termguicolors = true
