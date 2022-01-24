59cbe4b515ee721d7e8a9b6ea86326b26694e830
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2022-01-24 19:28:05 +0100

    Add more list select mappings

--- lua/my/mappings.lua
+++ lua/my/mappings.lua
@@ -1,6 +1,6 @@
-local nnoremap = require "my.utils".nnoremap
-local cnoremap = require "my.utils".cnoremap
-local tnoremap = require "my.utils".tnoremap
+local nnoremap = require("my.utils").nnoremap
+local cnoremap = require("my.utils").cnoremap
+local tnoremap = require("my.utils").tnoremap
 
 vim.cmd [[cnoremap <expr> <C-L> wildmenumode() ? "\<Down>\<Tab>" : "\<C-l>"]]
 vim.cmd [[cnoremap <expr> <C-H> wildmenumode() ? "\<Up>\<Tab>" : "\<C-h>"]]
@@ -12,10 +12,19 @@ nnoremap("<C-K>", "<Cmd>cprevious<CR>")
 nnoremap("<C-N>", "<Cmd>cnfile<CR>")
 nnoremap("<C-P>", "<Cmd>cpfile<CR>")
 nnoremap("m<C-M>", "<Cmd>update<bar>make<CR>")
-nnoremap("<space>", "<cmd>lua require'microscope.builtin'.args()<cr>")
-nnoremap(",,", "<cmd>lua require'microscope.builtin'.pickers()<cr>")
+
 nnoremap("sf", "<Cmd>Telescope find_files<CR>")
 nnoremap("sg", "<Cmd>Telescope live_grep<CR>")
 nnoremap("sd", "<Cmd>Telescope lsp_document_symbols<CR>")
+nnoremap(
+    "so",
+    "<cmd>lua require'telescope.builtin'.oldfiles(require'telescope.themes'.get_dropdown{ previewer = false })<CR>"
+)
+nnoremap("se", "<Cmd>Telescope diagnostics bufnr=0<CR>")
+nnoremap("sE", "<Cmd>lua vim.diagnostic.setqflist()<cr>")
+nnoremap("sc", "<cmd>lua require'microscope.builtin'.cwd()<cr>")
+nnoremap("sb", "<cmd>lua require'microscope.builtin'.buffer_dir()<cr>")
+nnoremap("<space>", "<cmd>lua require'microscope.builtin'.args()<cr>")
+nnoremap("sa", "<cmd>lua require'microscope.builtin'.args()<cr>")
 
 tnoremap("<C-_>", [[<C-\><C-N>]])
